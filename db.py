fd13e52db78a54fd92146f61d3c6f40036f4acbc
Dax Deniega <161933956+MASTER-DAX@users.noreply.github.com>
2025-11-21 19:53:56 +0800

    Update db.py

--- db.py
+++ db.py
@@ -1,31 +1,22 @@
 # db.py
 import os
 from pymongo import MongoClient
-from pymongo.server_api import ServerApi
 
-# ==========================
-# MONGODB CONNECTION STRING
-# ==========================
-# Read from Render's environment variable
+# Read MongoDB URI from Render environment variable
 MONGO_URI = os.getenv("MONGO_URI")
 
-if not MONGO_URI:
-    raise ValueError("⚠️ MONGO_URI environment variable is not set!")
+client = MongoClient(MONGO_URI)
+db = client["smart_cottage"]
+users_col = db["users"]
 
-# ==========================
-# CONNECT TO MONGO ATLAS
-# ==========================
-client = MongoClient(MONGO_URI, server_api=ServerApi('1'))
 
-try:
-    client.admin.command('ping')
-    print("✅ Connected to MongoDB Atlas!")
-except Exception as e:
-    print("❌ MongoDB Connection Error:", e)
+def get_user(uid):
+    return users_col.find_one({"uid": uid})
 
-# ==========================
-# DATABASE + COLLECTIONS
-# ==========================
-db = client["smart_cottage"]     # database name
-users_col = db["users"]          # store users
-logs_col = db["sensors"]         # store scan logs
+
+def register_user(uid, name, email):
+    users_col.insert_one({
+        "uid": uid,
+        "name": name,
+        "email": email
+    })
