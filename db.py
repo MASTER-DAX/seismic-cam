94a99025842f9cbcedcc9c26a2f2e68568e5e436
Dax Deniega <161933956+MASTER-DAX@users.noreply.github.com>
2025-11-21 12:13:28 +0800

    Create db.py

---
+++ db.py
@@ -0,0 +1,26 @@
+# db.py
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+# ==========================
+# MONGODB CONNECTION STRING
+# ==========================
+MONGO_URI = "mongodb+srv://daxdeniega16:13541ASAka@cluster0.u2nctpk.mongodb.net/?appName=Cluster0"
+
+# ==========================
+# CONNECT TO MONGO ATLAS
+# ==========================
+client = MongoClient(MONGO_URI, server_api=ServerApi('1'))
+
+try:
+    client.admin.command('ping')
+    print("✅ Connected to MongoDB Atlas!")
+except Exception as e:
+    print("❌ MongoDB Connection Error:", e)
+
+# ==========================
+# DATABASE + COLLECTIONS
+# ==========================
+db = client["rfid_system"]     # database name
+users_col = db["users"]        # store users
+logs_col = db["logs"]          # store scan logs
