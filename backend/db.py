a3b97363956ff450fffa5dc8f75548fdd8ef6ea9
Dax Deniega <161933956+MASTER-DAX@users.noreply.github.com>
2026-01-21 00:54:15 +0800

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,3 +1,4 @@
+
 from pymongo import MongoClient
 import os
 from datetime import datetime
@@ -17,6 +18,7 @@ except Exception as e:
     raise
 
 db = client["rfid_system"]
+   # ✅ MATCH URI
 
 users = db["users"]
 taps = db["taps"]
@@ -26,27 +28,31 @@ taps = db["taps"]
 # ------------------------
 
 def find_user_by_name_and_employee(name, employee_id):
-    return users.find_one(
-        {
-            "name": {"$regex": f"^{name}$", "$options": "i"},
-            "employee_id": employee_id
-        },
-        {"_id": 0}
-    )
+    try:
+        return users.find_one(
+            {
+                "name": {"$regex": f"^{name}$", "$options": "i"},
+                "employee_id": employee_id
+            },
+            {"_id": 0}
+        )
+    except Exception as e:
+        print("DB ERROR:", e)
+        return None
+
 
 def register_user(doc):
     doc["created_at"] = datetime.utcnow()
     users.replace_one({"uid": doc["uid"]}, doc, upsert=True)
     return True
 
+
 def find_user_by_uid(uid):
     return users.find_one({"uid": uid}, {"_id": 0})
 
+
 def trigger_buzzer_event(uid):
-    taps.insert_one({
-        "uid": uid,
-        "ts": datetime.utcnow()
-    })
+    taps.insert_one({"uid": uid, "ts": datetime.utcnow()})
 
 # ------------------------
 # DASHBOARD STATS
